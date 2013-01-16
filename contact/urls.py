884eb5c3a36e3557ac61043ae8fab9e9f9eb4614
chris lyon tech <chris.lyon.tech@gmail.com>
2013-01-16 10:50:22 +0100

    Correction des modifs et delete sur hg le contact/views et contact/urls sont de bon exemple de generique

--- contact/urls.py
+++ contact/urls.py
@@ -3,11 +3,13 @@
 from django.conf.urls import patterns, include, url
 from contact.models import Contact, ContactForm
 
+BASE_TMPL='contact/tmpl'
+
 urlpatterns = patterns('contact.views',
 	## Url standard 
-	url(r'^$', 'liste', { 'model':Contact, 'template':'contact/tmpl/liste.html', 'p':10, }, name='liste'),
-	url(r'^cr/$', 'create', { 'form':ContactForm, 'template':'contact/tmpl/form.html', }, name='cr'),
-	url(r'^mo/(\d+)$', 'modif', { 'model':Contact, 'form':ContactForm, 'template':'tst/tmpl/form.html', }, name='mo'),
-	url(r'^del/(\d+)/$', 'delete', { 'model':Contact, 'template':'tst/tmpl/delete.html', }, name='del'),
+	url(r'^$', 'liste', { 'model':Contact, 'template':BASE_TMPL+'/liste.html', 'p':10, }, name='liste'),
+	url(r'^cr/$', 'create', { 'form':ContactForm, 'template':BASE_TMPL+'/form.html', }, name='cr'),
+	url(r'^mo/(\d+)$', 'modif', { 'model':Contact, 'form':ContactForm, 'template':BASE_TMPL+'/form.html', }, name='mo'),
+	url(r'^del/(\d+)/$', 'delete', { 'model':Contact, 'template':BASE_TMPL+'/delete.html', }, name='del'),
 	#url(r'^pop$', 'populate', name='pop'),
 	)
